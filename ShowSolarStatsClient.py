af3231f7c509ea1f9527d2c309871a5eddea827b
Mark S Burgunder <mark@budafoods.com.au>
2018-04-29 17:21:06 +1000

    Adde command line processing to client... First version that could be integrated with solar monitoring scripts

--- ShowSolarStatsClient.py
+++ ShowSolarStatsClient.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
+import argparse
 import rpyc
 
+parser = argparse.ArgumentParser(description="Client to send solar data to Raspberry Pi to display")
+parser.add_argument("--server", action="store", default="localhost")
+parser.add_argument("--port", action="store", default="8000", type=int)
+parser.add_argument("--potential", action="store", required=True)
+parser.add_argument("--energy", action="store", required=True)
+parser.add_argument("--powernow", action="store", required=True)
+parser.add_argument("--power5min", action="store", required=True)
+args = parser.parse_args()
+
 if __name__ == "__main__":
-	c = rpyc.connect("localhost", 8000)
+	c = rpyc.connect(args.server, args.port)
 	stats = {}
-	stats['potential'] = 26.4
-	stats['energy'] = 11200
-	stats['powernow'] = 2753
-	stats['power5min'] = 3214
+	stats['potential'] = args.potential
+	stats['energy'] = args.energy
+	stats['powernow'] = args.powernow
+	stats['power5min'] = args.power5min
 
 	c.root.update_stats(stats)
