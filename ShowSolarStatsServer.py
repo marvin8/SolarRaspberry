066b6f1882a2d054296c66d14a446b5bd077a7e4
marvin8 <mark@budafoods.com.au>
2018-04-29 05:24:15 +0000

    Initial version of working rpyc communication

--- ShowSolarStatsServer.py
+++ ShowSolarStatsServer.py
@@ -4,10 +4,8 @@ import pifacecommon
 import pifacecad
 from threading import Barrier
 
-import socket
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import urllib
-import time
+import rpyc
+from rpyc.utils.server import ThreadedServer
 
 class StatsDisplay(object):
 	def __init__(self, cad):
@@ -32,62 +30,32 @@ class StatsDisplay(object):
 		self.cad.lcd.write("{power5min} W".format(
 			power5min=self.stats['power5min']))
 
+class DisplayServer(rpyc.Service):
 
-class ListeningServer(BaseHTTPRequestHandler):
+	def __init__(self, service):
+		self.cad=pifacecad.PiFaceCAD()
+		self.display = StatsDisplay(self.cad)
+		pass
 
-	#	GET is for clients geting the predi
-	def do_GET(self):
-		self.send_response(200)
-		self.wfile.write(bytes("<p>You accessed path: %s</p>" % self.path, "utf-8"))
+	def on_connect(self):
+		print("Connected")
+		pass
 
-	#	POST is for submitting data.
-	def do_POST(self):
+	def on_disconnect(self):
+		self.display.close()
+		print("Disconnceted")
+		pass
 
-		print( "incomming http: ", self.path )
-
-		content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-		print("headers: ", self.headers)
-		post_data = self.rfile.read(content_length) # <--- Gets the data itself
-		parseQS = urllib.parse.parse_qs(post_data.decode('UTF-8'))
-		print("parseQS: ", parseQS)
-		print("potential: ", parseQS['potential'][0])
-		print("energy: ", parseQS['energy'][0])
-		print("powernow: ", parseQS['powernow'][0])
-		print("power5min: ", parseQS['power5min'][0])
-	
-		self.send_response(200)
+	def exposed_update_stats(self, stats):
+		print("Disply to be updated with stats: ", stats)
+		self.display.update(stats)
+		print("Disply updated!")
 
 
 if __name__ == "__main__":
-	stats = {}
-	stats['potential'] = 26.4
-	stats['energy'] = 11200
-	stats['powernow'] = 2753
-	stats['power5min'] = 3214
 
-	hostName = ""
 	hostPort = 8000
 
-	cad = pifacecad.PiFaceCAD()
-	statsdisplay = StatsDisplay(cad)
-	statsdisplay.update(stats)
-
-#	server = HTTPServer((hostName, hostPort), ListeningServer(statsdisplay))
-	server = HTTPServer((hostName, hostPort), ListeningServer)
-
-	server.serve_forever()
-
-	global end_barrier
-	end_barrier = Barrier(2)
-
-	# wait for button press
-	switchlistener = pifacecad.SwitchEventListener(chip=cad)
-	switchlistener.register(4, pifacecad.IODIR_ON, end_barrier.wait)
-	switchlistener.activate()
-	end_barrier.wait()
-
-	#exit
-	server.server_close()
-	statsdisplay.close()
-	switchlistener.deactivate()
+	t = ThreadedServer(DisplayServer, port=hostPort)
+	t.start()
 
