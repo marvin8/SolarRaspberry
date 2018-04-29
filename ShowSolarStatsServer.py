aec25ae5b078c73b70d9e924706a65b92ea9bd15
marvin8 <mark@budafoods.com.au>
2018-04-29 07:56:15 +0000

    Formatting output on display

--- ShowSolarStatsServer.py
+++ ShowSolarStatsServer.py
@@ -21,13 +21,13 @@ class StatsDisplay(object):
 	def update(self, stats):
 		self.stats = stats
 		self.cad.lcd.clear()
-		self.cad.lcd.write("{potential} V  ".format(
+		self.cad.lcd.write("{potential:>4} V  ".format(
 			potential=self.stats['potential']))
-		self.cad.lcd.write("{energy} Wh\n".format(
+		self.cad.lcd.write("{energy:>5} Wh\n".format(
 			energy=self.stats['energy']))
-		self.cad.lcd.write("{powernow} W  ".format(
+		self.cad.lcd.write("{powernow:>4} W  ".format(
 			powernow=self.stats['powernow']))
-		self.cad.lcd.write("{power5min} W".format(
+		self.cad.lcd.write("{power5min:>5} W".format(
 			power5min=self.stats['power5min']))
 
 class DisplayServer(rpyc.Service):
