aa997e26f963e7adb77cc43fdc48d659ac1ccb11
marvin8 <mark@budafoods.com.au>
2018-05-05 17:55:47 +1000

    Added full paths to commands in restart shell script and sample crontab entry

--- runSolarStatsServer.sh
+++ runSolarStatsServer.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+restartEvery=$1
+
 while true; do
-	tmux new-session -d -s SolarStats ./ShowSolarStatsServer.py --port 8000
-	pid=$(ps aux | grep Solar | grep -v grep | cut -b 08-14 | tr -s [:blank:])
+	/usr/bin/tmux new-session -d -s SolarStats ./ShowSolarStatsServer.py --port 8000
+	pid=$(/bin/ps aux | /bin/grep Solar | /bin/grep python3 | /usr/bin/cut -b 08-14 | /usr/bin/tr -s [:blank:])
 	
-	sleep 30m
-	kill $pid
+	/bin/sleep $restartEvery
+	/bin/kill $pid
+	/bin/sleep 5s
 done
